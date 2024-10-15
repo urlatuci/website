79b78a9d7447c9145688fdfab5b47c0ad2be7067
Carlos Valdez <cvaldezh@uci.edu>
2024-10-15 16:50:04 -0700

    refactor(index.tsx): replace Ojos link with docs link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.module.css';
 export default function Home(): JSX.Element {
   return (
     <div className={styles.temporary_home}>
-      <p>You're early! Give us a second while we figure this thing out. Meanwhile, check out the <a href="https://ojosproject.org/" rel="noopener noreferrer" target="_blank">Ojos Project</a>!</p>
+      <p>You're early! We're still setting up the homepage. Meanwhile, check <a href="/docs/">our documentation</a>!</p>
 
       <p>Want to contribute? Check out <a href="https://github.com/urlatuci/website/" rel="noopener noreferrer" target="_blank">the repo</a>.</p>
     </div>
