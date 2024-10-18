4162c183cdf87c57ced2060a0132487be8d3713e
Carlos Valdez <cvaldezh@uci.edu>
2024-10-18 11:37:26 -0700

    feat(index.tsx): replace docs link with URL Proposal link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,11 @@ export default function Home(): JSX.Element {
   return (
     <Layout>
       <div className={styles.temporary_home}>
-        <p>You're early! We're still setting up the homepage. Meanwhile, check <a href="/docs/">our documentation</a>!</p>
+        <p>You're early! We're still setting up the homepage.</p>
 
-        <p>Want to contribute? Check out <a href="https://github.com/urlatuci/website/" rel="noopener noreferrer" target="_blank">the repo</a>.</p>
+        <p>Here to submit a proposal? Read <a href="/docs/proposals/">the URL Proposal Guidelines</a>!</p>
+
+        <p>Want to help build the website? Check out <a href="https://github.com/urlatuci/website/" rel="noopener noreferrer" target="_blank">the GitHub repo</a>.</p>
       </div>
     </Layout>
     
