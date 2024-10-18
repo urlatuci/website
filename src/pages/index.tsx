a576c81056547044cddaca8c09957f362ec08910
Carlos Valdez <cvaldezh@uci.edu>
2024-10-18 11:35:32 -0700

    fix(index.tsx): add layout to placeholder text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import styles from './index.module.css';
+import Layout from '@theme/Layout';
 
 export default function Home(): JSX.Element {
   return (
-    <div className={styles.temporary_home}>
-      <p>You're early! We're still setting up the homepage. Meanwhile, check <a href="/docs/">our documentation</a>!</p>
+    <Layout>
+      <div className={styles.temporary_home}>
+        <p>You're early! We're still setting up the homepage. Meanwhile, check <a href="/docs/">our documentation</a>!</p>
 
-      <p>Want to contribute? Check out <a href="https://github.com/urlatuci/website/" rel="noopener noreferrer" target="_blank">the repo</a>.</p>
-    </div>
+        <p>Want to contribute? Check out <a href="https://github.com/urlatuci/website/" rel="noopener noreferrer" target="_blank">the repo</a>.</p>
+      </div>
+    </Layout>
+    
     
   );
 }
